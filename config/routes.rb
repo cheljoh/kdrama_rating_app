614fc7fa41840f2f62ce688b927ebd7aaed2779f
Chelsea Johnson <cheljoh@gmail.com>
2016-02-25 17:07:45 -0700

    Added authentication for user
    Added feature tests for registering login and logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root to: "users#index"
   resources :users, only: [:new, :create, :show]
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 end
--- spec/features/user_can_register_spec.rb
+++ spec/features/user_can_register_spec.rb
@@ -7,12 +7,12 @@ RSpec.feature "user can register" do
     #i am redirect to my user show page
     visit '/'
     click_on "Register"
-
     fill_in "Username", with: "chelsea"
     fill_in "Password", with: "password"
     click_on "Submit"
     user = User.last
-    assert_equal "/users/#{user.id}", current_path
+    expect(page).to have_current_path(user_path(user))
+
 
     expect(page).to have_content("Welcome chelsea")
 
