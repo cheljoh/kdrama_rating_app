469633cf858e9b0aa5085c8b1fc73458eaf5613f
Chelsea Johnson <cheljoh@gmail.com>
2016-02-29 08:52:03 -0700

    changed tests

--- spec/features/user_can_log_out_spec.rb
+++ spec/features/user_can_log_out_spec.rb
@@ -4,13 +4,13 @@ RSpec.feature "user can log out" do
   scenario "logged in user can log out" do
     user = User.create(username: "Molly", password: "blah")
     visit login_path
-    fill_in "Username", with: "Admir"
+    fill_in "Username", with: "Molly"
     fill_in "Password", with: "blah"
     click_on "Submit"
 
     visit '/'
     click_on 'Logout'
-
+    save_and_open_page
     expect(page).to_not have_content("Welcome #{user.username}")
   end
 end
